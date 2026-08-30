ed137c3d83335f90d554d48539760c2e7fb219b6
Rafael <gonalo.rafael@email.com>
2026-08-30 18:16:13 -0300

    remove gif

--- src/components/hero/Characters.tsx
+++ src/components/hero/Characters.tsx
@@ -1,6 +1,5 @@
 import skeleton from '../../assets/skeleton.webp'
 import frog from '../../assets/frog.webp'
-import butterflys from '../../assets/butterflys.gif'
 
 export default function Characters() {
   return (
@@ -15,30 +14,10 @@ export default function Characters() {
         />
       </div>
 
-      <div className="max-md:w-1/5 w-1/7 absolute bottom-0 left-0 z-40">
-        <img
-          src={butterflys}
-          alt="butterflies"
-          className="w-full"
-          loading="lazy"
-          decoding="async"
-        />
-      </div>
-
       <div className="shadow2 max-md:w-3/5 w-2/7 absolute bottom-0 right-0 z-40">
         <img
           src={frog}
           alt="frog"
-          className="w-full"
-          loading="lazy"
-          decoding="async"
-        />
-      </div>
-
-      <div className="max-md:w-1/5 w-1/7 absolute bottom-0 right-0 z-40">
-        <img
-          src={butterflys}
-          alt="butterflies"
           className="w-full"
           loading="lazy"
           decoding="async"
